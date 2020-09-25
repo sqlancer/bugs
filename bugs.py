ed15978d1f3178fb17dc2c7e18233ac041a959e3
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-09-25 10:09:45 +0200

    Add a command to format the file and format it

--- bugs.py
+++ bugs.py
@@ -4,7 +4,7 @@ import sys
 
 
 parser = argparse.ArgumentParser(prog='bugs.py')
-parser.add_argument('action', choices=['check'])
+parser.add_argument('action', choices=['check', 'format'])
 args = parser.parse_args()
 
 f = open("bugs.json", "r")
@@ -16,8 +16,14 @@ def check():
     correctly_formatted = original_content == formatted_content
     sys.exit(0 if correctly_formatted else -1)
 
+def format_json():
+	formatted_content = json.dumps(parsed_content, indent=4) + '\n'
+	f.close()
+	fw = open("bugs.json", "w")
+	fw.write(formatted_content)
 
 action = args.action
 {
-    'check' : check
+    'check' : check,
+    'format' : format_json
 }[action]()
